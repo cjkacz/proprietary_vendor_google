df6aa1f5dbca6b5d633bdc626074413e0a4319fa
Christian Oder <myself5@carbonrom.org>
2017-10-03 23:26:11 +0200

    Update blobs to OPR3.170623.008

---
+++ sailfish/BoardConfigVendor.mk
@@ -0,0 +1,2 @@
+# [2017-10-03] Auto-generated file, do not edit
+
---
+++ sailfish/BoardConfigVendorPartial.mk
@@ -0,0 +1,6 @@
+# [2017-10-03] Auto-generated file, do not edit
+
+TARGET_BOARD_INFO_FILE := vendor/google/sailfish/vendor-board-info.txt
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_VENDORIMAGE_PARTITION_SIZE := 314572800
+BOARD_SEPOLICY_DIRS += device/google/marlin/sepolicy/verizon
